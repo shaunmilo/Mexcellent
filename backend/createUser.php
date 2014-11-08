c53420ddc56861e19d5d79648fff30ac779cb123
Shaun Milosevich <shaunmilo18@gmail.com>
2014-11-08 14:54:52 -0500

    User registration fix

--- backend/createUser.php
+++ backend/createUser.php
@@ -1,6 +1,6 @@
 <?php
 
-include("includes/config.php");
+include("../includes/config.php");
 $fname= $_REQUEST['firstName'];
 $lname= $_REQUEST['lastName'];
 $email= $_REQUEST['email'];
@@ -10,7 +10,7 @@ $billingAddress= $_REQUEST['billingAddress'];
 $billingCity= $_REQUEST['billingCity'];
 $billingState= $_REQUEST['billingState'];
 $billingZip= $_REQUEST['billingZip'];
-$shippingAddress= $_REQUEST['shippingAddres'];
+$shippingAddress= $_REQUEST['shippingAddress'];
 $shippingState= $_REQUEST['shippingState'];
 $shippingZip= $_REQUEST['shippingZip'];
 $cardNumber= $_REQUEST['cardNumber'];
@@ -20,8 +20,7 @@ $cardFirstname= $_REQUEST['cardFirstName'];
 $cardLastname= $_REQUEST['cardLastName'];
 
 
-$sql = "INSERT INTO Users (First Name, Last Name, Email, Password, Phone Number, Billing Address, Billing City, Billing State, Billing Zip, Shipping Address, Shipping City, Shipping State, Shipping Zip, Card Number, Expiration, Security Code, Card FName, Card LName)
-VALUES ('$fname', '$lname', '$email', '$password', '$phoneNumber', '$billingAddress', '$billingCity', '$billingState', '$billingzip', '$shippingAddress', '$shippingState', '$shippingZip', '$cardNumber', '$expiration', '$securityCode', '$cardFirstName', '$cardLastName')";
+$sql = "INSERT INTO Users (FirstName, LastName, Email, Password, PhoneNumber, BillingAddress, BillingCity, BillingState, BillingZip, ShippingAddress, ShippingCity, ShippingState, ShippingZip, CardNumber, Expiration, SecurityCode, CardFName, CardLName) VALUES ('', '$fname', '$lname', '$email', '$password', '$phoneNumber', '$billingAddress', '$billingCity', '$billingState', '$billingzip', '$shippingAddress', '$shippingState', '$shippingZip', '$cardNumber', '$expiration', '$securityCode', '$cardFirstName', '$cardLastName')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully. <a href='../registration.php'>Click here</a> to go back.";
