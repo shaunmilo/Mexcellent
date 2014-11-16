4d0bde11599cc7f0e19f093e8dc87e79e9bcb552
sgaray889 <sgaray889@gmail.com>
2014-11-16 16:30:29 -0500

    header
    change it into an unordered list

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,13 @@
 <div id="header">
 	<div id="site-container" class="grid">
-    	<header class="row">
-        	<a href="home.php" class="c2" id="title" >Mexcellent</a>
-            <a href="about.php" class="c3" id="menuButton">About Us</a>
-            <a href="catalog.php" class="c3" id="menuButton">View our Menu</a>
-            <a class="c2" id="myAccount" href="client.php">My Account</a>
-            <a class="c1" id="shoppingBag" href="cart.php">Bag(<?php echo count($_SESSION['product']); ?>)</a>
+    	<header class="row" id="navigation">
+        	<ul>
+                <li><a href="home.php"  id="title" >Mexcellent</a></li>
+                <li><a href="about.php"  id="about">About Us</a></li>
+                <li><a href="catalog.php"  class="menuButton">View our Menu</a></li>
+                <li><a  id="myAccount" href="client.php"class="menuButton" >My Account</a></li>
+                <li><a  id="shoppingBag" href="cart.php" class="menuButton">Bag(<?php echo count($_SESSION['product']); ?>)</a></li>
+            </ul>
         </header>
     </div>
 </div>
